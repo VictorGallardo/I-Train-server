333797685ee4a516bf63e3b8ba3a4b213ae45fc8
Víctor Gallardo <vbubobuho@gmail.com>
2021-05-17 22:00:27 +0200

    Nueva subida

--- routes/list.routes.ts
+++ routes/list.routes.ts
@@ -13,7 +13,7 @@ listRoutes.get('/', [validateToken], async (req: any, res: Response) => {
 
     let page = Number(req.query.page) || 1;
     let skip = page - 1;
-    skip = skip * 10;
+    skip = skip * 13;
 
     const body = req.body;
     body.user = req.user._id;
@@ -22,7 +22,7 @@ listRoutes.get('/', [validateToken], async (req: any, res: Response) => {
         .find(body)           // Busca por id user
         .sort({ _id: -1 })    // Ordena
         .skip(skip)           // Paginación
-        .limit(10)            // Muestra solo 10    
+        .limit(13)            // Muestra solo 10    
         .exec()               // Ejecuta
 
     // Respuesta    
